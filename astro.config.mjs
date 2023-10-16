dc9f4273dbf47c76b55f92ea716af58a390afa0a
Skyler Kimura <skyler8@hawaii.edu>
2023-10-16 12:09:33 -1000

    publish to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cri-lab.github.io',
+  base: '/crc-documentation',
   integrations: [
     starlight({
       title: "CRC Documentation",
